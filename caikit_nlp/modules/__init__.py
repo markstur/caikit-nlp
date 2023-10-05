927f0f50cd45538bdb5a04e52849456b9a3389d8
markstur <mark.sturdevant@ibm.com>
2023-10-04 21:45:08 -0700

    Adding reranker
    Signed-off-by: markstur <mark.sturdevant@ibm.com>

--- caikit_nlp/modules/__init__.py
+++ caikit_nlp/modules/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # Local
-from . import text_classification, text_generation, token_classification
+from . import reranker, text_classification, text_generation, token_classification
